329caf032195481a8e181ca9447afbacfc5b98f1
Nguyễn Thái  Học <hoc.nguyen@fiisoft.com>
2021-03-18 16:52:46 +0700

    fix import

--- lib/domain/search_usecase.dart
+++ lib/domain/search_usecase.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:built_collection/built_collection.dart';
 import 'package:flutter_github_search_rx_redux/domain/search_repo.dart';
-import 'package:meta/meta.dart';
 
 import 'repo_item.dart';
 
